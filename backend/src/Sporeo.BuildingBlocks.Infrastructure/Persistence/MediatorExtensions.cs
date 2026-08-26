52ebbf9cca25b6cab9e239ae1c648f2509275a77
Maciej Rybka <maciek.rybka2004@gmail.com>
2026-08-26 14:09:10 +0200

    feat: create dispatch domain events mediator extension

---
+++ backend/src/Sporeo.BuildingBlocks.Infrastructure/Persistence/MediatorExtensions.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Sporeo.BuildingBlocks.Domain.Events;
+
+namespace Sporeo.BuildingBlocks.Infrastructure.Persistence;
+
+public static class MediatorExtensions
+{
+    public static async Task DispatchDomainEventsAsync(this IPublisher publisher, DbContext context)
+    {
+        var domainEntities = context.ChangeTracker
+            .Entries<IHasDomainEvents>()
+            .Where(x => x.Entity.DomainEvents != null && x.Entity.DomainEvents.Count != 0)
+            .ToList();
+
+        if (domainEntities.Count == 0)
+            return;
+
+        var domainEvents = domainEntities
+            .SelectMany(x => x.Entity.DomainEvents)
+            .ToList();
+
+        domainEntities.ForEach(entry => entry.Entity.ClearDomainEvents());
+
+        foreach (var domainEvent in domainEvents)
+            await publisher.Publish(domainEvent);
+    }
+}
