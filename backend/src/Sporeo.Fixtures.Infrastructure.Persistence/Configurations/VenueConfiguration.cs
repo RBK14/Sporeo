455c2a5a6ca8b842233700fa8bd53356a0aa3825
Maciej Rybka <maciek.rybka2004@gmail.com>
2026-09-04 12:23:40 +0200

    refactor: remove computed colum and index creation from Venues configuration, add index for ExternalProviderId column

--- backend/src/Sporeo.Fixtures.Infrastructure.Persistence/Configurations/VenueConfiguration.cs
+++ backend/src/Sporeo.Fixtures.Infrastructure.Persistence/Configurations/VenueConfiguration.cs
@@ -6,12 +6,16 @@ using Sporeo.Fixtures.Domain.Venues.ValueObjects;
 
 namespace Sporeo.Fixtures.Infrastructure.Persistence.Configurations;
 
+/// <summary>
+/// EF Core mapping for <see cref="Venue"/>, including owned address/coordinates and the indexed geography location.
+/// </summary>
 internal class VenueConfiguration : IEntityTypeConfiguration<Venue>
 {
+    /// <inheritdoc />
     public void Configure(EntityTypeBuilder<Venue> builder)
     {
         builder.ToTable("venues");
-        
+
         builder.HasKey(x => x.Id);
         builder.Property(x => x.Id)
             .HasConversion(
@@ -25,36 +29,58 @@ internal class VenueConfiguration : IEntityTypeConfiguration<Venue>
 
         builder.OwnsOne(x => x.Address, address =>
         {
-            address.Property(a => a.Street).HasColumnName("Street").HasMaxLength(200);
-            address.Property(a => a.City).HasColumnName("City").HasMaxLength(100);
-            address.Property(a => a.Country).HasColumnName("Country").HasMaxLength(100);
+            address.Property(a => a.Street)
+                .HasColumnName("Street")
+                .HasMaxLength(200)
+                .IsRequired(false);
+
+            address.Property(a => a.City)
+                .HasColumnName("City")
+                .HasMaxLength(100)
+                .IsRequired();
+
+            address.Property(a => a.Country)
+                .HasColumnName("Country")
+                .HasMaxLength(100)
+                .IsRequired();
         });
+        builder.Navigation(x => x.Address).IsRequired(false);
 
         builder.OwnsOne(x => x.Coordinates, coordinates =>
         {
             coordinates.Property(c => c.Latitude)
                 .HasColumnName("Latitude")
-                .HasColumnType("decimal(9,6)");
+                .HasColumnType("decimal(9,6)")
+                .HasConversion(
+                    value => (decimal)value,
+                    value => (double)value)
+                .IsRequired();
 
             coordinates.Property(c => c.Longitude)
                 .HasColumnName("Longitude")
-                .HasColumnType("decimal(9,6)");
+                .HasColumnType("decimal(9,6)")
+                .HasConversion(
+                    value => (decimal)value,
+                    value => (double)value)
+                .IsRequired();
         });
+        builder.Navigation(x => x.Coordinates).IsRequired(false);
 
+        // Regular geography column maintained by VenueLocationInterceptor.
+        // Spatial index is created manually in the InitialCreate migration.
         builder.Property<Point>("Location")
-            .HasColumnType("geography")
-            .HasComputedColumnSql(
-                "CASE WHEN [Latitude] IS NOT NULL AND [Longitude] IS NOT NULL THEN geography::Point([Latitude], [Longitude], 4326)",
-                stored: true);
-
-        builder.HasIndex("Location")
-               .HasDatabaseName("IX_Venues_Location");
+            .HasColumnType("geography");
 
         builder.Property(x => x.ExternalProviderName)
             .HasMaxLength(100);
 
         builder.Property(x => x.ExternalProviderId)
             .HasMaxLength(100);
+
+        builder.HasIndex(x => new { x.ExternalProviderName, x.ExternalProviderId })
+            .IsUnique()
+            .HasFilter("[ExternalProviderName] IS NOT NULL AND [ExternalProviderId] IS NOT NULL AND [IsDeleted] = 0")
+            .HasDatabaseName("IX_venues_ExternalProvider");
 
         builder.Property(x => x.IsManuallyEdited)
             .IsRequired();
