d299700e6406c76ce8b05d179c3b638141a166fb
Maciej Rybka <maciek.rybka2004@gmail.com>
2026-09-04 12:29:26 +0200

    feat: configure startup projects

--- backend/src/Sporeo.Fixtures.API/Program.cs
+++ backend/src/Sporeo.Fixtures.API/Program.cs
@@ -1,6 +1,18 @@
+using Sporeo.Fixtures.Application;
+using Sporeo.Fixtures.Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.AddServiceDefaults();
+builder.Services.AddApplication();
+builder.Services.AddPersistence(builder.Configuration);
+
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+    await app.Services.InitializeFixturesDatabaseAsync();
+
+app.MapDefaultEndpoints();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
