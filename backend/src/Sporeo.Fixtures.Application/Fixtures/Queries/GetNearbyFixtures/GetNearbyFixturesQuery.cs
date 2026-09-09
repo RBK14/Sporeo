61e8fb2c106c140bf9238156d9b511ec2be03c34
Maciej Rybka <maciek.rybka2004@gmail.com>
2026-09-09 21:14:16 +0200

    feat: add GetNearbyFixtures query

---
+++ backend/src/Sporeo.Fixtures.Application/Fixtures/Queries/GetNearbyFixtures/GetNearbyFixturesQuery.cs
@@ -0,0 +1,12 @@
+﻿using Sporeo.BuildingBlocks.Application.Abstractions.Execution;
+using Sporeo.BuildingBlocks.Application.Pagination;
+using Sporeo.Fixtures.Application.Fixtures.Queries.Common;
+
+namespace Sporeo.Fixtures.Application.Fixtures.Queries.GetNearbyFixtures;
+
+public sealed record GetNearbyFixturesQuery(
+    double Latitude,
+    double Longitude,
+    double RadiusInMeters,
+    PaginationParams Pagination,
+    FixtureFilters Filters) : IQuery<PagedResult<NearbyFixtureListItemResponse>>;
---
+++ backend/src/Sporeo.Fixtures.Application/Fixtures/Queries/GetNearbyFixtures/NearbyFixtureListItemResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Sporeo.Fixtures.Application.Fixtures.Queries.GetNearbyFixtures;
+
+public sealed record NearbyFixtureListItemResponse(
+    Guid Id,
+    string Name,
+    DateTimeOffset StartDate,
+    string SportName,
+    string? LeagueName,
+    double DistanceInMeters);
