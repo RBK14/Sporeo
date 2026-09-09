6e603582c37f57beb2c2456505530e722a3bd8b8
Maciej Rybka <maciek.rybka2004@gmail.com>
2026-09-09 12:39:48 +0200

    feat: add base pagination

---
+++ backend/src/Sporeo.BuildingBlocks.Application/Pagination/PagedResults.cs
@@ -0,0 +1,16 @@
+﻿namespace Sporeo.BuildingBlocks.Application.Pagination;
+
+public class PagedResult<T>
+{
+    public IReadOnlyList<T> Items { get; }
+    public int TotalCount { get; }
+    public bool HasNextPage => TotalCount > Items.Count;
+    public PaginationParams Pagination { get; }
+
+    public PagedResult(IEnumerable<T> items, int totalCount, PaginationParams pagination)
+    {
+        Items = items.ToList();
+        TotalCount = totalCount;
+        Pagination = pagination;
+    }
+}
---
+++ backend/src/Sporeo.BuildingBlocks.Application/Pagination/PaginationParams.cs
@@ -0,0 +1,6 @@
+﻿namespace Sporeo.BuildingBlocks.Application.Pagination;
+
+public record PaginationParams(int Page, int PageSize)
+{
+    public int Offset => (Page - 1) * PageSize;
+}
