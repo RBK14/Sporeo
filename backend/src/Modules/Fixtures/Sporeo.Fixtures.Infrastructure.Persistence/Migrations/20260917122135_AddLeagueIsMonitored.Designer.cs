4775a5a0684dfaae2349515827b7d119f428b668
Maciej Rybka <maciek.rybka2004@gmail.com>
2026-09-17 14:20:28 +0200

    feat: add isMonitored property do League entity and database

--- backend/src/Modules/Fixtures/Sporeo.Fixtures.Infrastructure.Persistence/Migrations/20260917122135_AddLeagueIsMonitored.Designer.cs
+++ backend/src/Modules/Fixtures/Sporeo.Fixtures.Infrastructure.Persistence/Migrations/20260917122135_AddLeagueIsMonitored.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NetTopologySuite.Geometries;
 using Sporeo.Fixtures.Infrastructure.Persistence.Context;
@@ -12,9 +13,11 @@ using Sporeo.Fixtures.Infrastructure.Persistence.Context;
 namespace Sporeo.Fixtures.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(FixturesDbContext))]
-    partial class FixturesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260917122135_AddLeagueIsMonitored")]
+    partial class AddLeagueIsMonitored
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/src/Modules/Fixtures/Sporeo.Fixtures.Infrastructure.Persistence/Migrations/20260917122135_AddLeagueIsMonitored.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sporeo.Fixtures.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLeagueIsMonitored : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsMonitored",
+                table: "leagues",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Leagues_ActiveMonitored",
+                table: "leagues",
+                column: "IsMonitored",
+                filter: "[IsMonitored] = 1 AND [IsDeleted] = 0");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Leagues_ActiveMonitored",
+                table: "leagues");
+
+            migrationBuilder.DropColumn(
+                name: "IsMonitored",
+                table: "leagues");
+        }
+    }
+}
